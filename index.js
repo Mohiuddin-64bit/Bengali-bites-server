f001f79c145e99b905a3bc1d3ab6bb2d40b3771e
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-05-04 23:06:12 +0600

    Port Initialization

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const chefs = require("./data/chefs.json");
 app.use(cors());
+// Port Initialization
 const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send({ message: "Hello server running" });
