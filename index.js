f753e8f39d990f5a7841f04a8ecba92ee246a73a
Mohiuddin <mohiuddin.niddu@gmail.com>
2023-05-04 21:14:07 +0600

    af

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const chefs = require("./data/chefs.json");
+app.use(cors());
+const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send({ message: "Hello server running" });
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedRecipe = chefs.find((n) => n.id == id);
+  res.send(selectedRecipe);
+});
+
+
+app.listen(port, () => {
+  console.log("server is running ");
+});
